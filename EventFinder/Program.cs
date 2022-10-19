2cd25725e3645427e636044ddc041d88e10ec04a
edissonlcn <52835947+edissonlcn@users.noreply.github.com>
2022-10-19 12:14:02 -0300

    Initial commit

---
+++ EventFinder/Program.cs
@@ -0,0 +1,96 @@
+﻿
+using EventFinder.Business;
+using EventFinder.Models;
+using System.Text.Json;
+
+var events = new List<Event>{
+new Event{ Name = "Phantom of the Opera", City = "New York"},
+new Event{ Name = "Metallica", City = "Los Angeles"},
+new Event{ Name = "Metallica", City = "New York"},
+new Event{ Name = "Metallica", City = "Boston"},
+new Event{ Name = "LadyGaGa", City = "New York"},
+new Event{ Name = "LadyGaGa", City = "Boston"},
+new Event{ Name = "LadyGaGa", City = "Chicago"},
+new Event{ Name = "LadyGaGa", City = "San Francisco"},
+new Event{ Name = "LadyGaGa", City = "Washington"}
+};
+
+var customer = new Customer { Name = "Mr. Fake", City = "New York" };
+
+//TASK 1.
+//Write a LINQ query to find the events that are in the same city as the customer
+//and print the event name and city to the console.
+//The output should be:
+//Mr. Fake: Phantom of the Opera in New York
+//Mr. Fake: Metallica in New York
+//Mr. Fake: LadyGaGa in New York
+Console.WriteLine("---------TASK 1---------");
+var sameCityEvents = events.Where(e => e.City == customer.City);
+foreach (var localEvent in sameCityEvents)
+{
+    EventService.AddToEmail(customer, localEvent);
+}
+
+//TASK 2.
+//Write a code to find the closest events to the customer
+//The output should be:
+//Mr.Fake: Phantom of the Opera in New York
+//Mr. Fake: Metallica in New York
+//Mr. Fake: LadyGaGa in New York
+//Mr. Fake: LadyGaGa in Chicago(221 miles away)
+//Mr.Fake: LadyGaGa in Washington(347 miles away)
+Console.WriteLine("---------TASK 2---------");
+var closestEvents = new Dictionary<Event, int>();
+foreach (var item in events)
+{
+    closestEvents.Add(item, EventService.GetDistance(customer.City, item.City));
+}
+
+//TASK 5
+var closest5 = EventService.SortEvents(closestEvents, (x => x.Value), 5);
+
+for (var i = 0; i < closest5.Count(); i++)
+{
+    EventService.AddToEmail(customer, closest5.ElementAt(i).Key);
+}
+
+//TASK 3 / 4
+//Creates and array of task to get the distance from the API
+//cant run this without an actual api
+var closestEventsApi = new Dictionary<Event, int>();
+List<Task<HttpResponseMessage>> tasks = new List<Task<HttpResponseMessage>>();
+foreach (var item in events)
+{
+    Task<HttpResponseMessage> t = Task.Run(() => EventService.GetDistanceApi(customer.City, item.City));
+    tasks.Add(t);
+}
+Task.WaitAll(tasks.ToArray());
+for (var i = 0; i < tasks.Count; i++)
+{
+    var response = tasks[i];
+    //task returns 200
+    if (response.Result.IsSuccessStatusCode)
+    {
+        var responseStream = await response.Result.Content.ReadAsStreamAsync();
+        var distance = await JsonSerializer.DeserializeAsync<int>(responseStream);
+        closestEventsApi.Add(events[i], distance);
+    }
+    else
+    {
+        closestEventsApi.Add(events[i], int.MaxValue);
+    }    
+}
+
+//TASK 5
+var pricedEvents = new Dictionary<Event, int>();
+foreach (var item in events)
+{
+    //changing the value here.
+    pricedEvents.Add(item, EventService.GetPrice(item));
+}
+var priced5 = EventService.SortEvents(closestEvents, (x => x.Value), 5);
+for (var i = 0; i < priced5.Count(); i++)
+{
+    EventService.AddToEmail(customer, priced5.ElementAt(i).Key);
+}
+
---
+++ EventFinder/Services/EventService.cs
@@ -0,0 +1,74 @@
+﻿using EventFinder.Models;
+using System.Linq;
+using System.Text.Json;
+
+namespace EventFinder.Business
+{
+    public class EventService
+    {
+
+        public EventService()
+        {
+            
+        }
+    
+        public static void AddToEmail(Customer c, Event e, int? price = null)
+
+        {
+            var distance = GetDistance(c.City, e.City);
+            Console.Out.WriteLine($"{c.Name}: {e.Name} in {e.City}"
+            + (distance > 0 ? $" ({distance} miles away)" : "")
+            + (price.HasValue ? $" for ${price}" : ""));
+        }
+        public static int GetPrice(Event e)
+        {
+            return (AlphebiticalDistance(e.City, "") + AlphebiticalDistance(e.Name, "")) / 10;
+        }
+        public static int GetDistance(string fromCity, string toCity)
+        {
+            return AlphebiticalDistance(fromCity, toCity);
+        }
+         
+        //TASK 3 / TASK 4
+        
+        public static Task<HttpResponseMessage> GetDistanceApi(string fromCity, string toCity)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "URL_HERE");
+
+            var client = new HttpClient();
+            try
+            {
+                return client.SendAsync(request);
+            }
+            catch
+            {
+                return null!;
+            }
+
+        }
+
+        //TASK 5
+        public static IEnumerable<KeyValuePair<Event,int>> SortEvents(Dictionary<Event, int> events, Func<KeyValuePair<Event, int>,int> func,  int take)
+        {            
+            return events.OrderBy(func).Take(take);
+        }
+        
+        private static int AlphebiticalDistance(string s, string t)
+        {
+            var result = 0;
+            var i = 0;
+            for (i = 0; i < Math.Min(s.Length, t.Length); i++)
+            {
+                // Console.Out.WriteLine($"loop 1 i={i} {s.Length} {t.Length}");
+                result += Math.Abs(s[i] - t[i]);
+            }
+            for (; i < Math.Max(s.Length, t.Length); i++)
+            {
+                // Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
+                result += s.Length > t.Length ? s[i] : t[i];
+            }
+            return result;
+        }
+    }
+}
